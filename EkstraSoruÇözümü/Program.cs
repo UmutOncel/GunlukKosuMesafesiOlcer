8055a8d098ea34412591aa89596392d3a79029a5
UmutOncel <umutoncel91@gmail.com>
2023-09-12 10:09:19 +0300

    Add project files.

---
+++ EkstraSoruÇözümü/Program.cs
@@ -0,0 +1,187 @@
+﻿namespace EkstraSoruÇözümü
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Koşu mesafesi, adım boyu, dakikadaki adım sayısı girildiğinde koşu süresini hesaplayan uygulama.
+
+            try
+            {
+                Giriş();
+                float[] arrayAdımBoyu = new float[0];
+                int[] arrayAdımSayısı = new int[0];
+                float parkurUzunluğu = ParkurUzunluğunuAl();
+                int turSayısı = TurSayısınıAl();
+                for (int i = 0; i < turSayısı; i++)
+                {
+                    Console.WriteLine($"Tebrikler! {i + 1}. tur bitti. Şimdi tur verilerini girme zamanı.");
+                    int adımBoyuSantimetre = AdımBoyuAl();
+                    float adımBoyuMetre = SantimetreyiMetreyeÇevir(adımBoyuSantimetre);
+                    int adımSayısı = DakikadakiAdımSayısı();
+                    FloatDiziyeElemanEkle(ref arrayAdımBoyu, adımBoyuMetre);
+                    İntDiziyeElemanEkle(ref arrayAdımSayısı, adımSayısı);
+                }
+                float[] turSüresi = new float[turSayısı];
+                for (int i = 0; i < turSayısı; i++)
+                {
+                    turSüresi[i] = parkurUzunluğu / (arrayAdımBoyu[i] * arrayAdımSayısı[i]);
+                }
+                string mesaj = $"Bravo! Parkur etrafındaki {turSayısı} turu başarıyla tamamladınız. Şimdi tur sürelerine bakalım:";
+                DiziyiEkranaYazdır(mesaj, turSayısı, turSüresi);
+                mesaj = "Tur sürelerinin en hızlıdan yavaşa doğru sıralaması:";
+                TurSüreleriniSırala(mesaj, turSüresi);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Uygulama hakkında bilgi veren metot.
+        /// </summary>
+        private static void Giriş()
+        {
+            string mesaj = "Bu uygulama bir parkur etrafında koştuğunuzda tur sürelerini dakika olarak hesaplar ve en hızlıdan yavaşa doğru sıralar. Tek yapmanız gereken parkur uzunluğunu, adım boyunuzu ve dakikada attığını adım sayısını girmektir.";
+            Console.WriteLine(mesaj);
+        }
+
+        /// <summary>
+        /// Kullanıcıdan adım boyunu alan ve kontrolleri yapan metot.
+        /// </summary>
+        /// <returns></returns>
+        private static int AdımBoyuAl()
+        {
+            int intCevap;
+            Console.WriteLine();
+            Console.Write("Lütfen adım boyunuzu santimetre cinsinden giriniz: ");
+            string cevap = Console.ReadLine().Trim();
+            bool sayıMı = BuBirSayıMı(cevap, out intCevap);
+            return sayıMı ? intCevap : AdımBoyuAl();
+        }
+
+        /// <summary>
+        /// Kullanıcının girdiği değerin sayı olup olmadığını kontrol eden, sayı ise o sayıyı döndüren metot.
+        /// </summary>
+        /// <param name="strCevap"></param>
+        /// <param name="intCevap"></param>
+        /// <returns></returns>
+        private static bool BuBirSayıMı(string strCevap, out int intCevap)
+        {
+            bool geçersizDeğer;
+            if (geçersizDeğer = !int.TryParse(strCevap, out intCevap))
+            {
+                Console.WriteLine("Hatalı giriş yaptınız! Lütfen sadece sayısal veri giriniz.");
+                return false;
+            }
+            else
+                return int.TryParse(strCevap, out intCevap);
+        }
+
+        /// <summary>
+        /// Santimetreyi metreye çeviren metot.
+        /// </summary>
+        /// <param name="sayı"></param>
+        /// <returns></returns>
+        private static float SantimetreyiMetreyeÇevir(int sayı)
+        {
+            float metre = (float)sayı / 100;
+            return metre;
+        }
+
+        /// <summary>
+        /// Kullanıcıdan dakikadaki adım sayısını alan ve kontrolleri yapan metot.
+        /// </summary>
+        /// <returns></returns>
+        private static int DakikadakiAdımSayısı()
+        {
+            int intCevap;
+            Console.WriteLine();
+            Console.Write("Lütfen bir dakikada kaç adım attığınızı giriniz: ");
+            string cevap = Console.ReadLine().Trim();
+            bool sayıMı = BuBirSayıMı(cevap, out intCevap);
+            return sayıMı ? intCevap : DakikadakiAdımSayısı();
+        }
+
+        /// <summary>
+        /// Kullanıcıdan parkur uzunluğunu alan ve kontrolleri yapan metot.
+        /// </summary>
+        /// <returns></returns>
+        private static int ParkurUzunluğunuAl()
+        {
+            int intCevap;
+            Console.WriteLine();
+            Console.Write("Lütfen parkur uzunluğunu metre cinsinden giriniz: ");
+            string cevap = Console.ReadLine().Trim();
+            bool sayıMı = BuBirSayıMı(cevap, out intCevap);
+            return sayıMı ? intCevap : ParkurUzunluğunuAl();
+        }
+
+        /// <summary>
+        /// Kullanıcıdan atacağı tur sayısını alan metot.
+        /// </summary>
+        /// <returns></returns>
+        private static int TurSayısınıAl() 
+        {
+            int intCevap;
+            Console.WriteLine();
+            Console.Write("Lütfen parkur etrafında atacağınız tur sayısını giriniz: ");
+            string cevap = Console.ReadLine().Trim();
+            bool sayıMı = BuBirSayıMı(cevap, out intCevap);
+            return sayıMı ? intCevap : TurSayısınıAl();
+        }
+
+        /// <summary>
+        /// Integer diziye eleman ataması yapan metot.
+        /// </summary>
+        /// <param name="intArray"></param>
+        /// <param name="intSayı"></param>
+        private static void İntDiziyeElemanEkle(ref int[] intArray, int intSayı)
+        {
+            Array.Resize(ref intArray, intArray.Length + 1);
+            intArray[^1] = intSayı;
+        }
+
+        /// <summary>
+        /// Float diziye eleman ataması yapan metot.
+        /// </summary>
+        /// <param name="floatArray"></param>
+        /// <param name="sayı"></param>
+        private static void FloatDiziyeElemanEkle(ref float[] floatArray, float sayı)
+        {
+            Array.Resize(ref floatArray, floatArray.Length + 1);
+            floatArray[^1] = sayı;
+        }
+
+        /// <summary>
+        /// Diziyi ekrana yazdıran metot.
+        /// </summary>
+        /// <param name="mesaj"></param>
+        /// <param name="turSüresi"></param>
+        private static void DiziyiEkranaYazdır(string mesaj, int turSayısı, float[] turSüresi)
+        {
+            Console.WriteLine(mesaj);
+            for (int i = 0; i < turSayısı; i++)
+            {
+                Console.WriteLine($"{i+1}. tur süresi= {turSüresi[i]} dakika");
+            }
+        }
+
+        /// <summary>
+        /// Tur sürelerini en hızlıdan yavaşa doğru sıralayan metot. 
+        /// </summary>
+        /// <param name="mesaj"></param>
+        /// <param name="turSüresi"></param>
+        private static void TurSüreleriniSırala(string mesaj, float[] turSüresi) 
+        {
+            Console.WriteLine();
+            Array.Sort(turSüresi);
+            foreach (float dakika in turSüresi)
+            {
+                Console.Write($"{dakika} - ");
+            }
+        }
+    }
+}
---
+++ Soru/Program.cs
@@ -0,0 +1,167 @@
+﻿namespace Soru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Adım boyu, dakikadaki adım sayısı ve koşu süresi girildiğinde koşu mesafesini hesaplayan uygulama.
+
+            try
+            {
+                Giriş();
+                bool devamMı;
+                do
+                {
+                    int adımBoyuSantimetre = AdımBoyuAl();
+                    float adımBoyuMetre = SantimetreyiMetreyeÇevir(adımBoyuSantimetre);
+                    int adımSayısı = DakikadakiAdımSayısı();
+                    int koşuSüresiSaat = KoşuSüresiSaat();
+                    int koşuSüresi = SaatiDakikayaÇevir(koşuSüresiSaat);
+                    int koşuSüresiDakika = KoşuSüresiDakika();
+                    float koşuMesafesi = KoşuMesafesiHesapla(adımBoyuMetre, adımSayısı, koşuSüresi, koşuSüresiDakika);
+                    Console.WriteLine($"Koşu mesafeniz = {koşuMesafesi} metredir.");
+                    string soru = "Yeni bir koşu mesafesi hesaplamak ister misiniz? Yeni veri girişi için 'Evet' yazınız.";
+                    devamMı = EvetHayırSorusuSor(soru);
+                } while(devamMı);
+                Console.ReadLine();
+            }
+            catch (Exception u)
+            {
+                Console.WriteLine(u.Message);
+            }
+        }
+
+        /// <summary>
+        /// Uygulama hakkında bilgi veren metot.
+        /// </summary>
+        private static void Giriş()
+        {
+            string mesaj = "Bu uygulama günlük koşu mesafenizi ölçmek için tasarlanmıştır. Adım boyunuzu, dakikadaki adım sayınızı ve koşu sürenizi girdiğiniz takdirde koşu mesafeniz hesaplanacaktır.";
+            Console.WriteLine(mesaj);
+        }
+
+        /// <summary>
+        /// Kullanıcıdan adım boyunu alan ve kontrolleri yapan metot.
+        /// </summary>
+        /// <returns></returns>
+        private static int AdımBoyuAl()
+        {
+            int intCevap;
+            Console.WriteLine();
+            Console.Write("Lütfen adım boyunuzu santimetre cinsinden giriniz: ");
+            string cevap = Console.ReadLine().Trim();
+            bool sayıMı = BuBirSayıMı(cevap, out intCevap);
+            return sayıMı ? intCevap : AdımBoyuAl();
+        }
+
+        /// <summary>
+        /// Kullanıcının girdiği değerin sayı olup olmadığını kontrol eden, sayı ise o sayıyı döndüren metot.
+        /// </summary>
+        /// <param name="strCevap"></param>
+        /// <param name="intCevap"></param>
+        /// <returns></returns>
+        private static bool BuBirSayıMı(string strCevap, out int intCevap)
+        {
+            bool geçersizDeğer;
+            if (geçersizDeğer = !int.TryParse(strCevap, out intCevap))
+            {
+                Console.WriteLine("Hatalı giriş yaptınız! Lütfen sadece sayısal veri giriniz.");
+                return false;
+            }
+            else
+                return int.TryParse(strCevap, out intCevap);
+        }
+
+        /// <summary>
+        /// Santimetreyi metreye çeviren metot.
+        /// </summary>
+        /// <param name="sayı"></param>
+        /// <returns></returns>
+        private static float SantimetreyiMetreyeÇevir(int sayı) 
+        {
+            float metre = (float)sayı/100;
+            return metre;
+        }
+
+        /// <summary>
+        /// Kullanıcıdan dakikadaki adım sayısını alan ve kontrolleri yapan metot.
+        /// </summary>
+        /// <returns></returns>
+        private static int DakikadakiAdımSayısı()
+        {
+            int intCevap;
+            Console.WriteLine();
+            Console.Write("Lütfen bir dakikada kaç adım attığınızı giriniz: ");
+            string cevap = Console.ReadLine().Trim();
+            bool sayıMı = BuBirSayıMı(cevap, out intCevap);
+            return sayıMı ? intCevap : DakikadakiAdımSayısı();
+        }
+
+        /// <summary>
+        /// Kullanıcıdan koşu süresinin tam saat kısmını alan ve kontrolleri yapan metot.
+        /// </summary>
+        /// <returns></returns>
+        private static int KoşuSüresiSaat()
+        {
+            int intCevap;
+            Console.WriteLine();
+            Console.Write("Lütfen koşu sürenizin tam saat kısmını giriniz: ");
+            string cevap = Console.ReadLine().Trim();
+            bool sayıMı = BuBirSayıMı(cevap, out intCevap);
+            return sayıMı ? intCevap : KoşuSüresiSaat();
+        }
+
+        /// <summary>
+        /// Saati dakikaya çeviren metot.
+        /// </summary>
+        /// <param name="sayı"></param>
+        /// <returns></returns>
+        private static int SaatiDakikayaÇevir(int sayı) 
+        {
+            int dakika = sayı * 60;
+            return dakika;
+        }
+
+        /// <summary>
+        /// Kullanıcıdan koşu süresinin tam saat kısmından arta kalan dakikasını alan ve kontrolleri yapan metot.
+        /// </summary>
+        /// <returns></returns>
+        private static int KoşuSüresiDakika()
+        {
+            int intCevap;
+            Console.WriteLine();
+            Console.Write("Lütfen koşu sürenizin tam saat kısmından arta kalan dakika kısmını giriniz: ");
+            string cevap = Console.ReadLine().Trim();
+            bool sayıMı = BuBirSayıMı(cevap, out intCevap);
+            return sayıMı ? intCevap : KoşuSüresiDakika();
+        }
+
+        /// <summary>
+        /// Koşu mesafesini hesaplayan metot.
+        /// </summary>
+        /// <param name="adımBoyu"></param>
+        /// <param name="adımSayısı"></param>
+        /// <param name="saat"></param>
+        /// <param name="dakika"></param>
+        /// <returns></returns>
+        private static float KoşuMesafesiHesapla(float adımBoyu, int adımSayısı, int saat, int dakika) 
+        {
+            float koşuMesafesi = adımBoyu * adımSayısı * (saat + dakika);
+            return koşuMesafesi;
+        }
+
+        /// <summary>
+        /// Kullanıcıya evet ya da hayır cevabı verebileceği bir soru soran metot.
+        /// </summary>
+        /// <param name="soru"></param>
+        /// <returns></returns>
+        private static bool EvetHayırSorusuSor(string soru)
+        {
+            Console.WriteLine();
+            Console.Write($"{soru} ");
+            string cevap = Console.ReadLine().Trim().ToLower();
+            cevap = cevap.Substring(0, 1);
+            return cevap == "e";
+        }
+    }
+}
